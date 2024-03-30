28a0ec4bc9cbacb3b085b2251e22ffeaf8896df5
好奇怪o <78187633+JunJie-zhang-o@users.noreply.github.com>
2024-03-30 22:45:42 +0800

    Update userError.py
    更新报错内容

--- mission/userError.py
+++ mission/userError.py
@@ -15,15 +15,15 @@ class UserWarning():
 Error53900 = UserError(53900, "气泵持续工作中,请检测是否漏气")
 
 
-Error53901 = UserError(53901, "气泵推出超时,请检测气压与对应的检测开关")
-Error53902 = UserError(53902, "气泵推回超时,请检测气压与对应的气缸")
+Error53901 = UserError(53901, "气缸推出超时,请检测气压与对应的气缸")
+Error53902 = UserError(53902, "气缸推回超时,请检测气压与对应的检测开关")
 Error53903 = UserError(53903, "")
 Error53904 = UserError(53904, "")
 Error53905 = UserError(53905, "")
 Error53906 = UserError(53906, "")
 Error53907 = UserError(53907, "")
 Error53908 = UserError(53908, "")
-Error53909 = UserError(53909, "AGV 与 Robot Modbus 心跳超时,请检测网络设置和网络链接")
+Error53909 = UserError(53909, "底盘 与 机器人通讯 心跳超时,请检测网络设置和网络链接")
 
 
 
